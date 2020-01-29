fb3b6ab01b8fd31520279c65fc2271fdf808c0db
taejoonJeon <taejjeon@gmail.com>
2020-01-29 22:15:56 +0900

    comment assignment

---
+++ nodejs/leave_comment/public/javascripts/board.js
@@ -0,0 +1,18 @@
+function show_result() {
+    var title = this.comment_title.value;
+    var content = this.comment_content.value;
+    var userid = this.comment_userid.value;
+    var email = this.comment_email.value;
+    var datetime = this.comment_datetime.value;
+}
+
+function reset_input() {
+    this.comment_title.value = "";
+    this.comment_content.value = "";
+    this.comment_userid.value = "";
+    this.comment_email.value = "";
+}
+
+function back_homepage() {
+
+}
---
+++ nodejs/leave_comment/routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  var db = req.db;
+  var collection = db.get('comments');
+  collection.find({}, {}, function (e, docs) {
+    res.render('index', {
+      title: '방명록 작성',
+      "articleList": docs
+    });
+  });
+});
+router.get('/newcomment', function (req, res) {
+  res.render('newcomment', {
+    title: "세부 내용 작성"
+  })
+});
+router.get('/<int>')
+router.post('/addcomment', function (req, res) {
+  var db = req.db;
+  var title = req.body.title;
+  var content = req.body.content;
+  var userid = req.body.userid;
+  var email = req.body.email;
+  var datetime = req.body.datetime;
+  var collection = db.get('comments');
+
+  collection.insert({
+    "comment_title": title,
+    "comment_content": content,
+    "comment_userid": userid,
+    "comment_email": email,
+    "comment_datetime": datetime
+  }, function (err, doc) {
+    if (err) {
+      res.send("There was a problem adding the information to the db.");
+    } else {
+      res.location("/");
+      res.redirect("/");
+    }
+  });
+})
+
+module.exports = router;
